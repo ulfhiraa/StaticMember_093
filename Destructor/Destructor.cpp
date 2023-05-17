de0ef17ddd7625f8b5e4a855676fb5ff0aa94fe7
ulfhiraa <studyiraa@gmail.com>
2023-05-17 19:37:10 +0700

    method untuk mengisi data dan proses perulangan hingga syarat terpenuhi

--- Destructor/Destructor.cpp
+++ Destructor/Destructor.cpp
@@ -31,3 +31,10 @@ void angka::cetakData() {
 		cout << i << " = " << arr[i] << endl;
 	}
 }
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
