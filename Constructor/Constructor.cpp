22655513dcee460280693b9e78f9f0d6adfdd983
ulfhiraa <studyiraa@gmail.com>
2023-05-17 19:00:05 +0700

    method untuk mencetak nim dan nama

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -32,6 +32,11 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nama = iNama;
 }
 
+void mahasiswa::cetak() {
+	cout << endl << " Nim = " << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
+
 int main() {
 	mahasiswa mhs1;
 	mahasiswa mhs2(20);
