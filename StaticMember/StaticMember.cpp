d9d04fa6297bc85baefea1be507d08e498f1da78
ulfhiraa <studyiraa@gmail.com>
2023-05-17 19:20:10 +0700

    class angka yang memiliki 2 variabel

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -10,6 +10,10 @@ public:
 
 	void setID();
 	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/}
+	static int getNim() { return nim; /*Definisi Function*/}
+
 	mahasiswa(string pnama) :nama(pnama) { setID();  } 
 };
 
@@ -32,11 +36,14 @@ int main() {
 	mahasiswa mhs3("Andi Kurniawan");
 	mhs3.nim = 20;
 	mahasiswa mhs4("Joko Purbo");
+	mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
 
 	mhs1.printAll();
 	mhs2.printAll();
 	mhs3.printAll();
 	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; // mengakses nim melalui static member function "getNim"
 
 	return 0;
 }
