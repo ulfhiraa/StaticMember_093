2c0caad805522819b14e5f849ebbdabe94ae15bf
ulfhiraa <studyiraa@gmail.com>
2023-05-17 19:40:59 +0700

    constructor pointer dengan 5 angka

--- Destructor/Destructor.cpp
+++ Destructor/Destructor.cpp
@@ -41,7 +41,7 @@ void angka::isiData() {
 
 int main() {
 	angka belajarcpp(3); //Constructor Dipanggil
-	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil 
 	delete ptrBelajarcpp; //Destructor Dipanggil
 
 	return 0;
