584b74a127b24902853858cf6dcbb25d7fe273b2
ulfhiraa <studyiraa@gmail.com>
2023-05-17 18:51:38 +0700

    constructor tanpa parameter

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -6,4 +6,15 @@ class mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = " ";
+}
